69d452269175c687f71b02b7abe7418eeee9b30a
Syeda Noorain <syeda23bcs10221@ms.sst.scaler.com>
2024-09-05 23:19:12 +0530

    edit and delete

--- src/components/FeatureBlog.js
+++ src/components/FeatureBlog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, Typography, CardActionArea } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const FeaturedBlogs = () => {
   const blogs = useSelector((state) => state.blog.blogs || []);
@@ -9,17 +10,19 @@ const FeaturedBlogs = () => {
     <div>
       {blogs.slice(0, 3).map((blog) => (
         <Card key={blog.id} variant="outlined" sx={{ marginBottom: 2 }}>
-          <CardContent>
-            <Typography variant="h5" component="div">
-              {blog.title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {new Date(blog.date).toLocaleDateString()}
-            </Typography>
-            <Typography variant="body1" sx={{ marginTop: 1 }}>
-              {blog.content.substring(0, 100)}...
-            </Typography>
-          </CardContent>
+          <CardActionArea component={Link} to={`/blog/${blog.id}`}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {blog.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {new Date(blog.date).toLocaleDateString()}
+              </Typography>
+              <Typography variant="body1" sx={{ marginTop: 1 }}>
+                {blog.content.substring(0, 100)}...
+              </Typography>
+            </CardContent>
+          </CardActionArea>
         </Card>
       ))}
     </div>
